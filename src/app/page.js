9b1d25810d55efacb2f549d70199f2f4a9ec0ec8
lexferndo <alexxfernandoo85@gmail.com>
2023-09-21 03:09:56 +0700

    add toggle navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,25 @@
+"use client";
+
 import Carousel from "@/components/home/carousel";
+import fetchData from "@/fetch";
+import { useEffect } from "react";
 
 export default function Home() {
+  const fetchProduct = async () => {
+    try {
+      const response = await fetchData("api/products", {
+        method: "GET",
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  });
+
   return (
     <>
       <Carousel />
