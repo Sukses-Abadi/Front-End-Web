4d57eebf53726264ec53f7f36b9700c1a61b4592
lexferndo <alexxfernandoo85@gmail.com>
2023-09-20 22:14:07 +0700

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Carousel from "@/components/home/carousel";
+
+export default function Home() {
+  return (
+    <>
+      <Carousel />
+      <h2 className=" bg-base-100 text-6xl">HOMEPAGE!</h2>
+    </>
+  );
+}
