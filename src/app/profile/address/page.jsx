cedf3b0ca1df8deb69efca4da4cc7e23e9f0f614
lexferndo <alexxfernandoo85@gmail.com>
2023-09-26 03:22:20 +0700

    add data address to modal update address

--- src/app/profile/address/page.jsx
+++ src/app/profile/address/page.jsx
@@ -66,11 +66,11 @@ export default function Address() {
         {/* You can open the modal using document.getElementById('ID').showModal() method */}
         <button
           className="w-full border-dotted border-2 border-primary px-3 py-4 text-sm font-semibold text-center text-primary rounded-sm hover:bg-primary hover:text-white"
-          onClick={() => document.getElementById("my_modal_3").showModal()}
+          onClick={() => document.getElementById("my_modal_1").showModal()}
         >
           Add Your Address
         </button>
-        <dialog id="my_modal_3" className="modal">
+        <dialog id="my_modal_1" className="modal">
           <div className="modal-box rounded-lg">
             <form method="dialog">
               <div className="flex flex-row pb-8">
@@ -194,17 +194,17 @@ export default function Address() {
               </div>
               <div className="flex">
                 <p className="text-md font-light text-primary">
-                  {address.street}
+                  Street : {address.street}
                 </p>
               </div>
               <div className="flex">
                 <p className="text-md font-light text-primary">
-                  {address.city.name}
+                  City : {address.city.name}
                 </p>
               </div>
               <div className="flex">
                 <p className="text-md font-light text-primary">
-                  {address.zip_code}
+                  Zip Code : {address.zip_code}
                 </p>
               </div>
 
@@ -213,9 +213,123 @@ export default function Address() {
                   Delete Address
                 </button>
 
-                <button className="text-sm font-normal text-red-600 px-3 py-4  hover:text-red-800">
+                <button
+                  className="text-sm font-normal text-red-600 px-3 py-4  hover:text-red-800"
+                  onClick={() =>
+                    document
+                      .getElementById(`my_modal_${address.id}`)
+                      .showModal()
+                  }
+                >
                   Edit Address
                 </button>
+
+                <dialog id={`my_modal_${address.id}`} className="modal">
+                  <div className="modal-box rounded-lg">
+                    <form method="dialog">
+                      <div className="flex flex-row pb-8">
+                        <h1 className="font-medium text-lg text-primary opacity-80">
+                          Edit Your Address
+                        </h1>
+                        {/* if there is a button in form, it will close the modal */}
+                        <button className="btn btn-sm btn-circle btn-ghost absolute text-lg right-2">
+                          ✕
+                        </button>
+                      </div>
+                    </form>
+                    <form className="space-y-6" action="#">
+                      <div>
+                        <label
+                          className="block uppercase tracking-wide text-primary text-xs font-bold mb-2"
+                          htmlFor="grid-username"
+                        >
+                          Name
+                        </label>
+                        <input
+                          className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                          id="grid-username"
+                          type="text"
+                          placeholder={address.name}
+                        />
+                      </div>
+                      <div>
+                        <label
+                          className="block uppercase tracking-wide text-primary text-xs font-bold mb-2"
+                          htmlFor="grid-username"
+                        >
+                          Street
+                        </label>
+                        <input
+                          className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                          id="grid-username"
+                          type="text"
+                          placeholder={address.street}
+                        />
+                      </div>
+                      <div className="flex flex-wrap -mx-3 mb-4">
+                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                          <label
+                            className="block uppercase tracking-wide text-primary text-xs font-bold mb-2"
+                            htmlFor="grid-first-name"
+                          >
+                            City
+                          </label>
+                          <div className="relative">
+                            <select
+                              className="appearance-none block w-full bg-gray-200 text-primary border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                              id="grid-city"
+                              name="city_id"
+                            >
+                              {cities.map((city) => {
+                                return (
+                                  <option
+                                    key={city.id}
+                                    value={city.id}
+                                    className="h-24"
+                                    selected={address.city.id === city.id}
+                                  >
+                                    {city.name}
+                                  </option>
+                                );
+                              })}
+                            </select>
+                            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                              <svg
+                                className="fill-current h-4 w-4"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 20 20"
+                              >
+                                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                              </svg>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="w-full md:w-1/2 px-3">
+                          <label
+                            className="block uppercase tracking-wide text-primary text-xs font-bold mb-2"
+                            htmlFor="grid-last-name"
+                          >
+                            Zip Code
+                          </label>
+                          <input
+                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="grid-last-name"
+                            type="text"
+                            placeholder={address.zip_code}
+                          />
+                        </div>
+                      </div>
+                      <div className="flex justify-between">
+                        <button
+                          type="submit"
+                          className="w-full border border-primary px-3 py-3 text-sm font-medium text-center text-primary rounded-lg hover:bg-primary hover:text-white"
+                        >
+                          Save
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </dialog>
               </div>
             </div>
           );
